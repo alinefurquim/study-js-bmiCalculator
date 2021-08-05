4a66f4df5a865a2bf5bbcce44ba093aa47cc48ee
Aline Furquim <aline1080@gmail.com>
2021-08-05 19:28:44 -0300

    Classification  if/else

--- script.js
+++ script.js
@@ -7,9 +7,27 @@ function bmi() {
     const result = document.getElementById('result');   
 
     if(name !== '' && height !== '' && weight !== '') {
-        alert ('Valor preenchido')
+        const bmiValue = (weight / (height * height)).toFixed(2);
+
+        let classification = '';
+
+        if (bmiValue < 18.5) {
+            classification = 'abaixo do peso';
+        } else if (bmiValue < 25) {
+            classification = 'com peso ideal. Parabéns!';
+        } else if (bmiValue < 30) {
+            classification = 'levemente acima do peso';
+        } else if (bmiValue < 35) {
+            classification = 'com obesidade grau I';
+        } else if (bmiValue < 40) {
+            classification = 'com obesidade grau II';
+        } else {
+            classification = 'com obesidade grau III. Cuidado!'
+        }
+
+        result.textContent = `${name} seu IMC é ${bmiValue} e você esta ${classification}`;
     } else {
-        alert ('Preencha todos campos')
+        result.textContent = 'Preencha todos os campos';
     }
 }
 
